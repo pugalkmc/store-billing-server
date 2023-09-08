41f0a24f244ee931154ba7b45c3b02a7cf6cb6ac
Pugazhenthi S <pugalkmc@gmail.com>
2023-09-08 12:25:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "https://saravana-agro-service.netlify.app"
+  origin: "https://master.d31qmkq85xz5r5.amplifyapp.com"
 }));
 const MONGODB_URI="mongodb+srv://pugalkmc:pugalsaran143@cluster0.dnr2yma.mongodb.net"
 mongoose.connect(MONGODB_URI, {
