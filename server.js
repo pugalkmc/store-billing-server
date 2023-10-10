073efc192f6f431c42f5c0474331b2b7272e3baf
Pugazhenthi S <pugalkmc@gmail.com>
2023-10-10 17:13:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "https://master.d31qmkq85xz5r5.amplifyapp.com"
+  origin: "https://master.d31qkq85xz5r5.amplifyapp.com"
 }));
 const MONGODB_URI="mongodb+srv://pugalkmc:pugalsaran143@cluster0.dnr2yma.mongodb.net"
 mongoose.connect(MONGODB_URI, {
